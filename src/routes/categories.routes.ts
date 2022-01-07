653fb4e848757aef74bcb7c2d285aaf64362e63e
verissimo-sn <jose.neto@minu.co>
2022-01-07 19:21:19 -0300

    feat: add categories route

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -3,15 +3,20 @@ import { Router } from 'express';
 import { CategoryRepository } from '../repositories/CategoriesRepository';
 
 const categoriesRoute = Router();
+const categoriesRepository = new CategoryRepository();
 
 categoriesRoute.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const categoriesRepository = new CategoryRepository();
-
   categoriesRepository.create({ name, description });
 
   return res.status(201).send();
+});
+
+categoriesRoute.get('/', (req, res) => {
+  const categories = categoriesRepository.list();
+
+  res.status(200).json(categories);
 });
 
 export { categoriesRoute };
