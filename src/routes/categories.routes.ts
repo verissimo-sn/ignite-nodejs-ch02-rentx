1aaa330c2f094499843cb8728d681d92505ea9f8
verissimo-sn <jose.neto@minu.co>
2022-01-08 07:58:08 -0300

    refactor: remove business rule and use categories service

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,20 +1,16 @@
 import { Router } from 'express';
 
-import { CategoryRepository } from '../repositories/CategoriesRepository';
+import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { CreateCategoryService } from '../services/CreateCategoryService';
 
 const categoriesRoute = Router();
-const categoriesRepository = new CategoryRepository();
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoute.post('/', (req, res) => {
   const { name, description } = req.body;
 
-  const categoryAlreadyExists = categoriesRepository.findByName(name);
-
-  if (categoryAlreadyExists) {
-    return res.status(400).json({ error: 'Category already exists' });
-  }
-
-  categoriesRepository.create({ name, description });
+  const createCategoryService = new CreateCategoryService(categoriesRepository);
+  createCategoryService.execute({ name, description });
 
   return res.status(201).send();
 });
