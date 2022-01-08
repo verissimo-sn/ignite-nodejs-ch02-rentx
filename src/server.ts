8765188b4a4f1faf9f19996484342fe79634c0e7
verissimo-sn <jose.neto@minu.co>
2022-01-08 08:50:16 -0300

    feat: add specifications routes on express

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import { categoriesRoute } from './routes/categories.routes';
+import { specificationsRoutes } from './routes/specifications.routes';
 
 const app = express();
 app.use(express.json());
 
 app.use('/categories', categoriesRoute);
+app.use('/specifications', specificationsRoutes);
 
 const port = 3333;
 app.listen(port, () => console.log(`Server is running on port ${port} !`));
