6784b1bed4f939cf9935bd09d470e965a08592c6
verissimo-sn <jose.neto@minu.co>
2022-01-08 08:48:10 -0300

    feat: add specifications repository

---
+++ src/modules/cars/repositories/SpecificationsRepository.ts
@@ -0,0 +1,31 @@
+import { SpecificationModel } from '../model/Specification';
+import {
+  ICreateSpecificationDto,
+  ISpecificationsRepository,
+} from './ISpecificationsRepository';
+
+class SpecificationRepository implements ISpecificationsRepository {
+  private specifications: SpecificationModel[];
+
+  constructor() {
+    this.specifications = [];
+  }
+
+  create({ name, description }: ICreateSpecificationDto): void {
+    const specification = new SpecificationModel();
+
+    Object.assign(specification, { name, description, created_at: new Date() });
+
+    this.specifications.push(specification);
+  }
+
+  findByName(name: string): SpecificationModel {
+    const specification = this.specifications.find(
+      (specification) => specification.name === name
+    );
+
+    return specification;
+  }
+}
+
+export { SpecificationRepository };
