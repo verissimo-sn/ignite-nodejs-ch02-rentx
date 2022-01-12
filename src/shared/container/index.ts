da9eccb773eeed09addea9ce6170153f7814bb7c
verissimo-sn <jose.neto@minu.co>
2022-01-12 09:12:14 -0300

    fix: fix repository name

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -2,7 +2,7 @@ import { container } from 'tsyringe';
 
 import { ICategoriesRepository } from '../../modules/cars/repositories/ICategoriesRepository';
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository';
-import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
+import { SpecificationsRepository } from '../../modules/cars/repositories/implementations/SpecificationsRepository';
 import { ISpecificationsRepository } from '../../modules/cars/repositories/ISpecificationsRepository';
 
 container.registerSingleton<ICategoriesRepository>(
@@ -11,6 +11,6 @@ container.registerSingleton<ICategoriesRepository>(
 );
 
 container.registerSingleton<ISpecificationsRepository>(
-  'SpecificationRepository',
-  SpecificationRepository
+  'SpecificationsRepository',
+  SpecificationsRepository
 );
