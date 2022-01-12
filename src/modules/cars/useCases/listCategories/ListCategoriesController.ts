35c1342c51b8c8a72e0a4fbcb17ac9e322a75dfb
verissimo-sn <jose.neto@minu.co>
2022-01-12 07:56:39 -0300

    feat: add async await

--- src/modules/cars/useCases/listCategories/ListCategoriesController.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesController.ts
@@ -5,8 +5,8 @@ import { ListCategoriesUseCase } from './ListCategoriesUseCase';
 class ListCategoriesController {
   constructor(private listCategoriesUseCase: ListCategoriesUseCase) {}
 
-  handle(req: Request, res: Response): Response {
-    const categories = this.listCategoriesUseCase.execute();
+  async handle(req: Request, res: Response): Promise<Response> {
+    const categories = await this.listCategoriesUseCase.execute();
 
     return res.json(categories);
   }
