d798afb85c322f907cd48fe03dac22c11b1dea65
verissimo-sn <jose.neto@minu.co>
2022-01-13 08:01:37 -0300

    feat: add appError handler

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import swaggerUi from 'swagger-ui-express';
+import 'express-async-errors';
 
+import { AppError } from './errors/AppError';
 import { router } from './routes';
 import swaggerConf from './swagger.json';
 
@@ -14,6 +16,19 @@ app.use(express.json());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConf));
 
 app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof AppError) {
+    return res.status(err.statusCode).json({
+      message: err.message,
+    });
+  }
+
+  return res.status(500).json({
+    status: 'error',
+    message: `Internal server error - ${err.message}`,
+  });
+});
 
 const port = 3333;
 app.listen(port, () => console.log(`Server is running on port ${port} !`));
