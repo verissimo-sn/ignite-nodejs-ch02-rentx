56811ef340eb5ab7d74c82fe87b5cb52973e3644
verissimo-sn <jose.neto@minu.co>
2022-01-16 12:23:53 -0300

    feat: add createCarController handle method

--- src/modules/cars/useCases/createCar/CreateCarController.ts
+++ src/modules/cars/useCases/createCar/CreateCarController.ts
@@ -1,3 +1,34 @@
-class CreateCarUseCase {}
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
 
-export { CreateCarUseCase };
+import { CreateCarUseCase } from './CreateCarUseCase';
+
+class CreateCarController {
+  async handle(req: Request, res: Response): Promise<Response> {
+    const {
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    } = req.body;
+
+    const createCarUseCase = container.resolve(CreateCarUseCase);
+
+    const car = await createCarUseCase.execute({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    return res.status(201).json(car);
+  }
+}
+
+export { CreateCarController };
