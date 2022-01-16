499052f5369d33c1d47ad62395057163c21e0f76
verissimo-sn <jose.neto@minu.co>
2022-01-16 12:22:56 -0300

    deat: add findByLicencePlate method in interface

--- src/modules/cars/repositories/ICarsRepository.ts
+++ src/modules/cars/repositories/ICarsRepository.ts
@@ -1,5 +1,7 @@
 import { ICreateCarDto } from '../dtos/ICreateCarDto';
+import { Car } from '../entities/Car';
 
 export interface ICarsRepository {
-  create(data: ICreateCarDto): Promise<void>;
+  create(data: ICreateCarDto): Promise<Car>;
+  findByLicencePlate(licensePlate: string): Promise<Car>;
 }
