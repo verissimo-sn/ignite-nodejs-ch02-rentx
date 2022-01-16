5f8279261fa056bf1b4448e39558c98b9f6bcce3
verissimo-sn <jose.neto@minu.co>
2022-01-16 19:17:45 -0300

    feat: add ensureAuthenticated and ensureAdmin middleware

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,11 +1,18 @@
 import { Router } from 'express';
 
+import ensureAdmin from '@middlewares/ensureAdmin';
+import ensureAuthenticated from '@middlewares/ensureAuthenticated';
 import { CreateSpecificationController } from '@modules/cars/useCases/createSpecification/CreateSpecificationController';
 
 const specificationsRoutes = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 
-specificationsRoutes.post('/', createSpecificationController.handle);
+specificationsRoutes.post(
+  '/',
+  ensureAuthenticated,
+  ensureAdmin,
+  createSpecificationController.handle
+);
 
 export { specificationsRoutes };
