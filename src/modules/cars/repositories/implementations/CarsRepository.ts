dbab0e1385e279c864ed4fdb75583666e08cf934
verissimo-sn <jose.neto@minu.co>
2022-01-16 12:23:32 -0300

    feat: create a new car

--- src/modules/cars/repositories/implementations/CarsRepository.ts
+++ src/modules/cars/repositories/implementations/CarsRepository.ts
@@ -1,9 +1,44 @@
+import { getRepository, Repository } from 'typeorm';
+
 import { ICreateCarDto } from '@modules/cars/dtos/ICreateCarDto';
+import { Car } from '@modules/cars/entities/Car';
 
 import { ICarsRepository } from '../ICarsRepository';
 
 class CarsRepository implements ICarsRepository {
-  create: (data: ICreateCarDto) => Promise<void>;
+  private repository: Repository<Car>;
+
+  constructor() {
+    this.repository = getRepository(Car);
+  }
+
+  async create({
+    name,
+    description,
+    daily_rate,
+    license_plate,
+    fine_amount,
+    brand,
+    category_id,
+  }: ICreateCarDto): Promise<Car> {
+    const car = this.repository.create({
+      name,
+      description,
+      daily_rate,
+      license_plate,
+      fine_amount,
+      brand,
+      category_id,
+    });
+
+    await this.repository.save(car);
+
+    return car;
+  }
+
+  async findByLicencePlate(licensePlate: string): Promise<Car> {
+    throw new Error('Method not implemented.');
+  }
 }
 
 export { CarsRepository };
