620e5ea77d1c91f8cc67fcccdc3c89ae48dcdeb8
verissimo-sn <jose.neto@minu.co>
2022-01-18 19:50:35 -0300

    feat: add createCarSpecificationController route

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -3,18 +3,27 @@ import { Router } from 'express';
 import ensureAdmin from '@middlewares/ensureAdmin';
 import ensureAuthenticated from '@middlewares/ensureAuthenticated';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableController } from '@modules/cars/useCases/listAvailableCars/ListAvailableController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCars = new ListAvailableController();
+const createCarSpecificationController = new CreateCarSpecificationController();
 
 carsRoutes.post(
   '/',
   ensureAuthenticated,
   ensureAdmin,
   createCarController.handle
+);
+
+carsRoutes.post(
+  '/specifications/:id',
+  ensureAuthenticated,
+  ensureAdmin,
+  createCarSpecificationController.handle
 );
 
 carsRoutes.get('/available', listAvailableCars.handle);
