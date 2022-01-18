102d7ce0f8a30ff7f3c4b22d49ebdd1fed34c1a0
verissimo-sn <jose.neto@minu.co>
2022-01-18 06:49:20 -0300

    feat: add findByLicencePlate and findAvailable method

--- src/modules/cars/repositories/implementations/CarsRepository.ts
+++ src/modules/cars/repositories/implementations/CarsRepository.ts
@@ -1,6 +1,7 @@
 import { getRepository, Repository } from 'typeorm';
 
 import { ICreateCarDto } from '@modules/cars/dtos/ICreateCarDto';
+import { IListAvailableDto } from '@modules/cars/dtos/IListAvailableDto';
 import { Car } from '@modules/cars/entities/Car';
 
 import { ICarsRepository } from '../ICarsRepository';
@@ -37,7 +38,33 @@ class CarsRepository implements ICarsRepository {
   }
 
   async findByLicencePlate(licensePlate: string): Promise<Car> {
-    throw new Error('Method not implemented.');
+    return this.repository.findOne({ license_plate: licensePlate });
+  }
+
+  async findAvailable({
+    name,
+    brand,
+    category_id,
+  }: IListAvailableDto): Promise<Car[]> {
+    const carsQuery = this.repository
+      .createQueryBuilder('cars')
+      .where('cars.available = :available', { available: true });
+
+    if (name) {
+      carsQuery.andWhere('name like :name', { name: `%${name}%` });
+    }
+
+    if (brand) {
+      carsQuery.andWhere('brand like :brand', { brand: `%${brand}%` });
+    }
+
+    if (category_id) {
+      carsQuery.andWhere('category_id = :category_id', { category_id });
+    }
+
+    const cars = await carsQuery.getMany();
+
+    return cars;
   }
 }
 
