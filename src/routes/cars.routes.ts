d1ef2e884db6efcf3996cbd207ebe548f9a2e8af
verissimo-sn <jose.neto@minu.co>
2022-01-18 06:50:48 -0300

    feat: add find available cars route

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express';
 import ensureAdmin from '@middlewares/ensureAdmin';
 import ensureAuthenticated from '@middlewares/ensureAuthenticated';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
+import { ListAvailableController } from '@modules/cars/useCases/listAvailableCars/ListAvailableController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
+const listAvailableCars = new ListAvailableController();
 
 carsRoutes.post(
   '/',
@@ -14,5 +16,7 @@ carsRoutes.post(
   ensureAdmin,
   createCarController.handle
 );
+
+carsRoutes.get('/available', listAvailableCars.handle);
 
 export { carsRoutes };
