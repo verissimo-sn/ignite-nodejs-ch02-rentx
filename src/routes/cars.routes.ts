b4fd1f57b6fc5a705e629897197cef82358a10ca
verissimo-sn <jose.neto@minu.co>
2022-01-19 07:56:07 -0300

    feat: add uploadCarImages route

--- src/routes/cars.routes.ts
+++ src/routes/cars.routes.ts
@@ -1,16 +1,22 @@
 import { Router } from 'express';
+import multer from 'multer';
 
+import uploadConfig from '@config/upload';
 import ensureAdmin from '@middlewares/ensureAdmin';
 import ensureAuthenticated from '@middlewares/ensureAuthenticated';
 import { CreateCarController } from '@modules/cars/useCases/createCar/CreateCarController';
 import { CreateCarSpecificationController } from '@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController';
 import { ListAvailableController } from '@modules/cars/useCases/listAvailableCars/ListAvailableController';
+import { UploadCarImagesController } from '@modules/cars/useCases/uploadCarImages/UploadCarImgesController';
 
 const carsRoutes = Router();
 
 const createCarController = new CreateCarController();
 const listAvailableCars = new ListAvailableController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImages = new UploadCarImagesController();
+
+const upload = multer(uploadConfig.upload('./tmp/cars'));
 
 carsRoutes.post(
   '/',
@@ -24,6 +30,14 @@ carsRoutes.post(
   ensureAuthenticated,
   ensureAdmin,
   createCarSpecificationController.handle
+);
+
+carsRoutes.post(
+  '/images/:id',
+  upload.array('images'),
+  ensureAuthenticated,
+  ensureAdmin,
+  uploadCarImages.handle
 );
 
 carsRoutes.get('/available', listAvailableCars.handle);
