e43ffbf53529665a3b4519da9e9c5f46a70ead2f
verissimo-sn <jose.neto@minu.co>
2022-01-22 07:59:33 -0300

    feat: add entity decorators

--- src/modules/rentals/entities/Rental.ts
+++ src/modules/rentals/entities/Rental.ts
@@ -1,30 +1,44 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { v4 as uuid } from 'uuid';
 
+@Entity('rentals')
 class Rental {
+  @PrimaryColumn()
   id?: string;
 
+  @Column()
   car_id: string;
 
+  @Column()
   user_id: string;
 
+  @Column()
   start_date: Date;
 
+  @Column()
   end_date: Date;
 
+  @Column()
   expected_return_date: Date;
 
+  @Column()
   total: number;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 
   constructor() {
     if (!this.id) {
       this.id = uuid();
-      this.start_date = new Date();
-      this.created_at = new Date();
-      this.updated_at = new Date();
     }
   }
 }
