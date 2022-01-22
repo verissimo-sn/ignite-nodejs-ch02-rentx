0be0aa1c87dfffa31ea99fe30b76ca41cab256a5
verissimo-sn <jose.neto@minu.co>
2022-01-22 10:00:26 -0300

    feat: add server inicialization

--- src/server.ts
+++ src/server.ts
@@ -1,36 +1,4 @@
-import 'reflect-metadata';
-import express, { NextFunction, Request, Response } from 'express';
-import swaggerUi from 'swagger-ui-express';
-import 'express-async-errors';
-
-import { AppError } from '@errors/AppError';
-
-import createConnection from './database';
-import { router } from './routes';
-import swaggerConf from './swagger.json';
-
-import './shared/container';
-
-createConnection();
-const app = express();
-app.use(express.json());
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerConf));
-
-app.use(router);
-
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-  if (err instanceof AppError) {
-    return res.status(err.statusCode).json({
-      message: err.message,
-    });
-  }
-
-  return res.status(500).json({
-    status: 'error',
-    message: `Internal server error - ${err.message}`,
-  });
-});
+import { app } from 'app';
 
 const port = 3333;
 app.listen(port, () => console.log(`Server is running on port ${port} !`));
