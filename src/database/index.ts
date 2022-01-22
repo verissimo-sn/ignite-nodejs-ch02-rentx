ee150a83d2a78bf04f640849a828a9a633f2b487
verissimo-sn <jose.neto@minu.co>
2022-01-22 09:59:35 -0300

    chore: confg hostname and dt test name

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,16 @@
+import 'dotenv/config';
 import { Connection, createConnection, getConnectionOptions } from 'typeorm';
 
-export default async (host = 'localhost'): Promise<Connection> => {
+export default async (host = 'database_rentx'): Promise<Connection> => {
   const defaultOptions = await getConnectionOptions();
 
   return createConnection(
     Object.assign(defaultOptions, {
-      host,
+      host: process.env.DB_HOST || host,
+      database:
+        process.env.NODE_ENV === 'test'
+          ? 'rentx_test'
+          : defaultOptions.database,
     })
   );
 };
