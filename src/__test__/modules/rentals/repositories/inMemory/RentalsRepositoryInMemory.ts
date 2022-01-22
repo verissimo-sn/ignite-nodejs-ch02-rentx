a8f687c48273bf8d2b01dc2b57f11748f99cd6ec
verissimo-sn <jose.neto@minu.co>
2022-01-22 10:00:57 -0300

    test: add start_date to create renal repository in memory

--- src/__test__/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
+++ src/__test__/modules/rentals/repositories/inMemory/RentalsRepositoryInMemory.ts
@@ -8,14 +8,15 @@ class RentalsRepositoryInMemory implements IRentalsRepository {
   async create({
     car_id,
     user_id,
-    expect_return_date,
+    expected_return_date,
   }: ICreateRentalDto): Promise<Rental> {
     const rental = new Rental();
 
     Object.assign(rental, {
       car_id,
       user_id,
-      expect_return_date,
+      expected_return_date,
+      start_date: new Date(),
     });
 
     this.rentals.push(rental);
