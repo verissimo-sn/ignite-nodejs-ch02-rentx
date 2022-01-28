5cdb24ae44e421f6b70cfd1ef73e0d0b193ccf78
verissimo-sn <jose.neto@minu.co>
2022-01-28 07:54:11 -0300

    feat: add EtherealMailProvider

--- src/shared/providers/index.ts
+++ src/shared/providers/index.ts
@@ -2,5 +2,11 @@ import { container } from 'tsyringe';
 
 import { IDateProvider } from '@shared/providers/DateProvider/IDateProvider';
 import { DayjsDateProvider } from '@shared/providers/DateProvider/implementations/DayjsDateProvider';
+import { IMailProvider } from '@shared/providers/MailProvider/IMailProvider';
+import { EtherealMailProvider } from '@shared/providers/MailProvider/implementations/EtherealMailProvider';
 
 container.registerSingleton<IDateProvider>('DateProvider', DayjsDateProvider);
+container.registerInstance<IMailProvider>(
+  'MailProvider',
+  new EtherealMailProvider()
+);
